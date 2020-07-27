dd93906319215c0044c50144d11d67ffeef99569
jd2426 <coupgo@icloud.com>
2020-07-27 02:27:21 -0400

    updates

--- index.js
+++ index.js
@@ -32,10 +32,15 @@ console.log(ageAsNumber)
 
 
 //Task d: Write a function to multiply a*b 
-let a = 10
-let b = 5
-let answer = a * b 
-console.log(answer)
+
+function multiply(firstNum, secondNum) {
+  firstNum = 5
+  secondNum = 10
+}
+
+console.log(firstNum * secondNum)
+
+// Ask Eddy about this one
 
 
 
@@ -44,9 +49,10 @@ console.log(answer)
 /************************************************************** Task 2 **************************************************************/
 //Age in Dog years
 //write a function that takes your age and returns it to you in dog years - they say that 1 human year is equal to seven dog years 
-let humanYear = 24
-let dogAge = humanYear / 7
-console.log(dogAge)
+function dogYears(a) {
+  return (a * 7)
+} 
+console.log(dogYears(10))
 
 
 
@@ -56,7 +62,7 @@ console.log(dogAge)
 //takes weight in pounds and age in years (note if the dog is a puppy the age will be a decimal) and returns the number of pounds of raw food to feed in a day.
 
 //feeding requirements
-// adult dogs at least 1 year 
+// adult dogs are at least 1 year 
 // up to 5 lbs - 5% of their body weight
 // 6 - 10 lbs - 4% of their body weight 
 // 11 - 15 lbs - 3% of their body weight 
@@ -68,7 +74,33 @@ console.log(dogAge)
 // 7 - 12 months 4% of their body weight
 
 // when you are finished invoke your function with the weight of 15 lbs and the age of 1 year - if your calculations are correct your result should be 0.44999999999999996
-  
+ function dogFeeder(pounds, age) {
+    if (age >=1) {
+      if (pounds <= 5) {
+        return .05 * pounds
+      } else if (pounds >= 6 && pounds <= 10) {
+        return .04 * pounds
+      } else if (pounds >=11 && pounds <=15) {
+        return .03 * pounds
+      } else {
+        return .02 * pounds
+      }
+       
+    }
+
+    if (age <1 && age >= 7 / 12) {
+       return .04 * pounds
+    } else if (age <7 / 12 && age >= 4 / 12) {
+      return .05 * pounds
+    }
+     else if (age <4 / 12 && age >= 2 / 12) {
+      return .10 * pounds
+    }
+    
+  }
+
+  console.log( "Your dogs needs" , dogFeeder(15, 1), "of food" )
+  console.log( "Your dogs needs" , dogFeeder(20, 3))
 
 
 
@@ -78,10 +110,44 @@ console.log(dogAge)
 // Your function should take a string (either rock paper or sissors)
 // it should return you won or you lost based on the rules of the game (you may need to look up the rules if you have not played before)
 // use math.random to determine the computers choice 
-// hint while you can complete this with only conditionals based on strings it may help to equate choice to a number 
+// hint while you can complete this with only conditionals based on strings it may help to equate choice to a number
 
+// rock = 1, paper = 2, scissors = 3
+   
+
+function winLoss(playerOne){
+  let computerPlayer = Math.floor(Math.random()*3 + 1);
+  let player = 0
   
-
+  if (playerOne.toLowerCase()==="rock") {
+    player = 1;
+  } else if (playerOne.toLowerCase()==="paper") {
+    player = 2;
+  } else {
+    player = 3;
+  } 
+// rock = 1, paper = 2, scissors = 3
+if (computerPlayer === 1 && player === 1) {
+  console.log("It's a tie");
+} else if (computerPlayer === 2 && player ===2) {
+  console.log("It's a tie")
+} else if (computerPlayer === 3 && player ===3) {
+  console.log("It's a tie")
+} else if (computerPlayer === 1 && player ===2) {
+  console.log("Player Wins")
+} else if (computerPlayer === 1 && player ===3) {
+  console.log("Computer Wins")
+} else if (computerPlayer === 2 && player ===3) {
+  console.log("Player Wins")
+} else if (computerPlayer === 2 && player ===1) {
+  console.log("Computer Wins")
+} else if (computerPlayer === 3 && player ===2) {
+  console.log("Computer Wins")
+} else if (computerPlayer === 3 && player ===1) {
+  console.log("Player Wins")
+} 
+  }
+  winLoss("rock")
   
 
 /************************************************************** Task 5 **************************************************************/
@@ -107,7 +173,14 @@ console.log(miles)
 // create a function called annoyingSong
 // the function should take a starting number as an argument and count down - at each iteration it should log (number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall`
   
+function annoyingSong (numBottles) {
+  console.log(`${numBottles}`)
 
+  for(let i = numBottles; i >= 0; i--) {
+    console.log(`${i}bottles of beer on the wall,  ${i} bottles of beer, take one down pass it around ${i-1} bottles of beer on the wall`)
+  }
+}
+annoyingSong(99);
 
 
 
@@ -120,6 +193,22 @@ console.log(miles)
 //60s should be D 
 //and anything below 60 should be F
   
+function gradeCalculator(score, grade) {
+  if(score>=90 && score<=100){
+    return grade = "A"
+  } else if (score >=80 && score<=90) {
+    return grade = "B"
+  } else if (score >=70 && score <=80) {
+    return grade = "C"
+  } else if (score >=60 && score >=70) {
+    return grade = "D"
+  } else {
+    return grade = "F"
+  }
+}
+console.log(gradeCalculator(54))
+
+// Ask eddy why anything over 100 is returning "D"
 
   
   
